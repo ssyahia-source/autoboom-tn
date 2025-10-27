d17600e5d5f37e87b8f7770679c6c003a28d011d
ssyahia-source <ssyahia@gmail.com>
2025-10-27 14:36:46 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,1 @@
-// next.config.js
-const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: { unoptimized: true },
-  assetPrefix: '',
-  basePath: '', // ← AJOUTEZ CETTE LIGNE
-}
 
-module.exports = nextConfig
